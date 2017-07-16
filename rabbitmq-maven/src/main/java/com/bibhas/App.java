072e351d97a97b2db470cf70900f305911b8c09b
Bibhas Abhishek <bibhas_01@hotmail.com>
2017-07-16 14:37:04 +0530

    rabbitmq maven code

---
+++ rabbitmq-maven/src/main/java/com/bibhas/App.java
@@ -0,0 +1,13 @@
+package com.bibhas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
