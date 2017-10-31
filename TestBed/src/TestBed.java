4d611e238ecf86b1590bcddb91c074a93362f3a4
Bibhas Abhishek <bibhas_01@hotmail.com>
2017-10-31 12:12:59 +0530

    master push

--- TestBed/src/TestBed.java
+++ TestBed/src/TestBed.java
@@ -7,12 +7,15 @@ import static java.util.stream.Collectors.toList;
  * [bibhas_01@hotmail.com]
  * 03 Oct 2017
  * https://github.com/bibhas-abhishek/projects/tree/master/TestBed
- * https://leetcode.com/problems/two-sum/description/
  */
 public class TestBed {
 
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        String a = "abc";
+        String b = "abc";
+        String c = new String("abc");
+        System.out.println(a == b);
+        System.out.println(a == c);
     }
 
 }
