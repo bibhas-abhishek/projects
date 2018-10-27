b1a8c9d951804237a242baf146f939a891c7a595
bibhas abhishek <bibhas_01@hotmail.com>
2018-10-28 02:23:32 +0530

    merge to master

--- SegmentTree/src/SegmentTree.java
+++ SegmentTree/src/SegmentTree.java
@@ -49,6 +49,9 @@ public class SegmentTree {
     }
 
     private int rangeMinQueryUtil(int[] segmentTree, int qLow, int qHigh, int low, int high, int pos) {
+        if (low > high)
+            return Integer.MAX_VALUE;
+
         if (qLow > high || qHigh < low)     // no overlap
             return Integer.MAX_VALUE;
 
@@ -58,6 +61,80 @@ public class SegmentTree {
         int mid = low + (high - low) / 2;
         int leftChild = 2 * pos + 1;
         int rightChild = 2 * pos + 2;
+        return Math.min(rangeMinQueryUtil(segmentTree, qLow, qHigh, low, mid, leftChild),
+                rangeMinQueryUtil(segmentTree, qLow, qHigh, mid + 1, high, rightChild));
+    }
+
+    public void updateSegmentTreeRangeLazy(int array[], int segmentTree[], int lazy[], int low, int high, int delta) {
+        updateSegmentTreeRangeLazy(segmentTree, lazy, low, high, delta, 0, array.length - 1, 0);
+    }
+
+    private void updateSegmentTreeRangeLazy(int segmentTree[], int lazy[], int uLow, int uHigh, int delta,
+                                            int low, int high, int pos) {
+        if (low > high)
+            return;
+
+        int leftChild = 2 * pos + 1;
+        int rightChild = 2 * pos + 2;
+
+        // make sure propagation is done at pos. update tree at pos and mark its children for propagation
+        if (lazy[pos] != 0) {
+            segmentTree[pos] += lazy[pos];
+            if (low != high) {
+                lazy[leftChild] += lazy[pos];
+                lazy[rightChild] = lazy[pos];
+            }
+            lazy[pos] = 0;
+        }
+
+        // no overlap
+        if (uLow > high || uHigh < low)
+            return;
+
+        // total overlap = (low, high) are contained in (uLow, uHigh)
+        if (uLow <= low && uHigh >= high) {
+            segmentTree[pos] += delta;
+            if (low != high) {
+                lazy[leftChild] += delta;
+                lazy[rightChild] += delta;
+            }
+            return;
+        }
+
+        int mid = low + (high - low) / 2;
+        updateSegmentTreeRangeLazy(segmentTree, lazy, uLow, uHigh, delta, low, mid, leftChild);
+        updateSegmentTreeRangeLazy(segmentTree, lazy, uLow, uHigh, delta, mid + 1, high, rightChild);
+        segmentTree[pos] = Math.min(segmentTree[leftChild], segmentTree[rightChild]);
+    }
+
+    public int rangeMinimumQueryLazy(int segmentTree[], int lazy[], int qLow, int qHigh, int length) {
+        return rangeMinimumQueryLazy(segmentTree, lazy, qLow, qHigh, 0, length - 1, 0);
+    }
+
+    private int rangeMinimumQueryLazy(int segmentTree[], int lazy[], int qLow, int qHigh, int low, int high, int pos) {
+        if (low > high)
+            return Integer.MAX_VALUE;
+
+        int leftChild = 2 * pos + 1;
+        int rightChild = 2 * pos + 2;
+
+        // make sure propagation is done at pos. update tree at pos and mark its children for propagation
+        if (lazy[pos] != 0) {
+            segmentTree[pos] += lazy[pos];
+            if (low != high) {
+                lazy[leftChild] += lazy[pos];
+                lazy[rightChild] = lazy[pos];
+            }
+            lazy[pos] = 0;
+        }
+
+        if (qLow > high || qHigh < low)     // no overlap
+            return Integer.MAX_VALUE;
+
+        if (qLow <= low && qHigh >= high)   // complete overlap
+            return segmentTree[pos];
+
+        int mid = low + (high - low) / 2;
         return Math.min(rangeMinQueryUtil(segmentTree, qLow, qHigh, low, mid, leftChild),
                 rangeMinQueryUtil(segmentTree, qLow, qHigh, mid + 1, high, rightChild));
     }
