244fcf1d145523567e2e74a1a1517b9945a3618c
BIbhas Abhishek <bibhas.abhishek91@gmail.com>
2020-11-01 19:48:17 +0530

    code refactors - merge to master

--- JavaTest/src/JavaTest.java
+++ JavaTest/src/JavaTest.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 public class JavaTest {
 
-    public String largestNumber(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return "";
-        }
-
-        String[] array = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
-        Arrays.sort(array, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        if (array[0].charAt(0) == '0') {
-            return "0";
-        }
-        return String.join("", array);
-    }
-
     public static void main(String[] args) {
-        int[] array = { 3, 30, 34, 5, 9 };
-        System.out.println(new JavaTest().largestNumber(array));
+        System.out.println("Hello World!");
     }
 }
